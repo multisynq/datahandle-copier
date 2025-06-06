edfff08d74fc99a87cc8444c3d09a582990d3c7e
Vanessa Freudenberg <vanessa@codefrau.net>
2025-06-06 17:27:32 +0200

    Initial commit

---
+++ datahandle-copier.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+'use strict';
+
+// This script copies data from an old shareable Croquet handle
+// to a new handle stored on a Multisynq file server.
+
+import { Model, View, Session, Data } from '@croquet/croquet';
+
+const src = process.argv[2];
+if (!src) {
+    console.error('Usage: node datahandle-copier.js <datahandle>');
+    process.exit(1);
+}
+
+if (!src[0] === '3') {
+    console.error('Can only decrypt shareable data handles! Aborting.');
+    process.exit(1);
+}
+
+const srcUrl = getUrl(src);
+const srcURL = new URL(srcUrl);
+if (!srcURL.hostname.endsWith('croquet.io')) {
+    console.log('Source URL host is', srcURL.hostname);
+    console.error('This script is meant to copy data from croquet.io to multisynq.io! Aborting.');
+    process.exit(1);
+}
+
+Session.join({
+    apiKey: '234567_Paste_Your_Own_API_Key_Here_7654321',
+    appId: 'io.multisynq.datahandle-copier',
+    name: 'default',
+    password: 'none',
+    model: Model,
+    view: View,
+    step: "manual",
+}).then(async session => {
+    console.log('Session started', session.id);
+
+    const srcHandle = Data.fromId(src);
+    console.log('Source URL:', getUrl(src));
+    const data = await session.data.fetch(srcHandle);
+    console.log('Data fetched:', data.length, 'bytes');
+
+    const dstHandle = await session.data.store(data.buffer, {shareable: true});
+    const dst = Data.toId(dstHandle);
+    console.log('Destination URL:', getUrl(dst));
+    console.log('Data copied to new handle:\n')
+    console.log(dst);
+
+    process.exit(0);
+});
+
+
+// Helper functions from Croquet's data.js
+
+function getUrl(id) {
+    const key = fromBase64Url(id.slice(1, 1 + 43));
+    const url = scramble(key, atob(fromBase64Url(id.slice(1 + 43))));
+    return url;
+}
+
+function scramble(key, string) {
+    if (!key) key = '*';
+    return string.replace(/[\s\S]/g, c => String.fromCharCode(c.charCodeAt(0) ^ key.charCodeAt(0)));
+}
+
+function fromBase64Url(base64url) {
+    return base64url.replace(/-/g, "+").replace(/_/g, "/").padEnd((base64url.length + 3) & ~3, "=");
+}
